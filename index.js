5d488f60ddede0cd82ad5f7b6e4e927e0e68a41b
Nicolas Torres <ntorresv@uni.pe>
2022-08-31 19:22:43 -0500

    AGREGO ARCHIVOS contenedor.js y productos.txt
    El archivo index.js contiene el servidor en express listo para subir a glitch

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const express = require('express')
 const app = express()
 
+const Contenedor = require('./contenedor.js')
+
+const db = new Contenedor('./productos.txt')
+
+app.get('/productos', async (req,res) => {
+    const productos = await db.getAll()
+    res.send(productos)
+})
+
+app.get('/productoRandom', async (req,res) => {
+    const productos = await db.getAll()
+    const tamaño = productos.length
+    const randomId = Math.floor(Math.random() * (tamaño - 1 + 1) + 1)
+    const producto = await db.getById(randomId)
+    res.send(producto)
+})
+
+
+
+const server = app.listen(8080, () => {
+    console.log(`Desafío: Servidor express iniciado - PORT: ${server.address().port}`)
+})
